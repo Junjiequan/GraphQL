f484f3032a2fff1cc181f673a122e831c264f7a1
mmmG <a331998513@gmail.com>
2022-07-08 20:10:13 +0200

    useAPI redo

--- src/hooks/useAPI.test.tsx
+++ src/hooks/useAPI.test.tsx
@@ -1,15 +1,15 @@
 import { renderHook } from '@testing-library/react-hooks';
 import { rest } from 'msw';
-import { setupServer } from 'msw/node';
+import { setupServer } from 'msw/lib/node';
 import '@testing-library/jest-dom';
 import useAPI from './useAPI';
-
 const URL = 'http://localhost:3000';
+
 const server = setupServer(
   rest.get(`${URL}/api`, (req, res, ctx) => {
     return res(
       ctx.json({
-        name: 'helllo jon',
+        name: 'helloooo',
       })
     );
   })
@@ -18,9 +18,8 @@ beforeAll(() => server.listen());
 afterEach(() => server.resetHandlers());
 afterAll(() => server.close());
 
-test('renders correct data', async () => {
+test('fetch data correctly', async () => {
   const { result, waitForNextUpdate } = renderHook(() => useAPI());
-
   await waitForNextUpdate();
-  expect(result.current).toStrictEqual({ name: 'helllo jon' });
+  expect(result.current).toEqual({ name: 'helloooo' });
 });
