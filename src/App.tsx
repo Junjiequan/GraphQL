e51e96135c4cf98f22b6a77bb87f0877b6702b68
Jay Quan <a331998513@gmail.com>
2022-12-09 16:18:32 +0100

    cheat sheet img changer

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,26 @@
 import Button from './components/Button/Button';
 import ReduxCounter from './components/Redux/ReduxCounter';
+import graphQL from './screenshots/graphQL.png';
+import test from './screenshots/test.png';
 
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [img, setImg] = useState(graphQL);
+  const onClick = (image: any) => {
+    setImg(image);
+  };
   return (
     <div className='App'>
-      <b>TestApp Buttons</b>
-      <Button label={'random button'} />
-      <b>Test Redux Store</b>
-      <ReduxCounter />
+      <div className='button-wrapper '>
+        <button onClick={() => onClick(graphQL)}>GraphQL CheatSheet</button>
+        <button onClick={() => onClick(test)}>Test</button>
+        <button onClick={() => onClick(test)}>GraphQL CheatSheet</button>
+      </div>
+      <div style={{ maxHeight: '100%' }}>
+        <img src={img} height='100%' style={{ objectFit: 'cover' }} />
+      </div>
     </div>
   );
 }
